2d302d3d881c571d450e60b4a47f25ff71e2e7d5
Himeon <90318893+Himeon@users.noreply.github.com>
2024-05-06 20:11:57 +0330

    Add files via upload

---
+++ pimint/command/commands.py
@@ -0,0 +1,27 @@
+
+from pimint.command.default.debug_command import debug_command
+from pimint.command.default.help_command import help_command
+from pimint.command.default.plugins_command import plugins_command
+from pimint.command.default.reload_command import reload_command
+from pimint.command.default.say_command import say_command
+from pimint.command.default.stop_command import stop_command
+from pimint.command.default.tell_command import tell_command
+from pimint.command.default.version_command import version_command
+from pimint.command.default.kick_command import kick_command
+from pimint.command.default.list_command import list_command
+from pimint.command.default.me_command import me_command
+from pimint.command.default.seed_command import seed_command
+from pimint.command.default.gamemode_command import gamemode_command
+from pimint.command.default.tell_command import tell_command
+from pimint.command.default.time_command import time_command
+from pimint.command.default.transfer_command import transfer_command
+from pimint.command.default.weather_command import weather_command
+from pimint.command.default.difficulty_command import difficulty_command
+
+__all__ = (
+    "debug_command", "help_command", "plugins_command", "reload_command",
+    "say_command", "stop_command", "tell_command", "version_command",
+    "kick_command", "list_command", "me_command", "seed_command",
+    "gamemode_command", "tell_command", "time_command", "transfer_command", "weather_command",
+    "difficulty_command"
+)
